634d4833a1f5afb970786f3a79530021a8587512
adrien <adrien.ruault@epfl.ch>
2017-11-28 18:05:56 +0100

    updated capital_coordinates.csv

--- project/panama_papers_aux.py
+++ project/panama_papers_aux.py
@@ -1,19 +1,32 @@
 import pandas as pd
-
-
+import folium
+import math
+import random
 
 
 
 
 class Item:
     """
-    type_ should be either intermediaries, officers, entities or addresses
+    type_ should be either Intermediary, Officer, Entity or Address
     """
+    
+    #static attribute containing the coordinates of the different capitals
+    country_coordinates_path = './res/capital_coordinates.csv'
+    
+    capital_coordinates_df = -1
+    
     def __init__(self, node_id, name, country, type_):
         self.node_id = node_id
         self.name = name
         self.country = country
         self.type = type_
+        if (self.country == -1):
+            self.latitude = -1
+            self.longitude = -1
+        else:
+            self._getCountryCoordinates(country)
+        print(self.country, 'latitude: {lat} / longitude: {lon}'.format(lat = self.latitude, lon = self.longitude))
         
     @classmethod
     def fromSingleLineDF(cls, single_line_df, type_):
@@ -26,26 +39,29 @@ class Item:
     # If the node is not in any of the DataFrames it outputs an empty node
     @classmethod
     def fromNodeId(cls, node_id, df_dictionary):
-        entities = df_dictionary['entities']
-        intermediaries = df_dictionary['intermediaries']
-        officers = df_dictionary['officers']
-        addresses = df_dictionary['addresses']
+        entities = df_dictionary['Entity']
+        intermediaries = df_dictionary['Intermediary']
+        officers = df_dictionary['Officer']
+        addresses = df_dictionary['Address']
     
-        type_ = 'entities'
+        type_ = 'Entity'
         node_df = entities[entities['node_id'] == node_id]
         if (node_df.empty):
-            type_ = 'intermediaries'
+            type_ = 'Intermediary'
             node_df = intermediaries[intermediaries['node_id'] == node_id]
         if (node_df.empty):
-            type_ = 'officers'
+            type_ = 'Officer'
             node_df = officers[officers['node_id'] == node_id]
         if (node_df.empty):
-            type_ = 'addresses'
+            type_ = 'Address'
             node_df = addresses[addresses['node_id'] == node_id]
         if (node_df.empty):
             item = cls.emptyInstance()
         else:
-            name = node_df['name'].values[0]
+            if (type_ == 'Address'):
+                name = node_df['address'].values[0]
+            else:
+                name = node_df['name'].values[0]
             country = node_df['countries'].values[0]
             item = cls(node_id, name, country, type_)
             
@@ -55,10 +71,44 @@ class Item:
     @classmethod
     def emptyInstance(cls):
         return cls(-1, -1, -1, -1)
-            
-            
-            
-            
+    
+    
+    @classmethod
+    def readCapitalCoordinates(cls):
+        if (isinstance(cls.capital_coordinates_df, int)):
+            cls.capital_coordinates_df = pd.read_csv(cls.country_coordinates_path)    
+    
+    
+    # define latitude and longitude of the country. 
+    # It is called in the Item constructor as soon as the country name is known
+    def _getCountryCoordinates(self, country):
+        #country_df = pd.read_csv('./res/country-capitals.csv')
+        if (isinstance(self.capital_coordinates_df, int)):
+            raise FileNotReadException('Please read the capital_coordinates.csv file before instantiating an Item object')
+        
+        # Some countries in the dataset are defined as several countries separated with a ';'
+        # To get coordinates anyway we parse the country name in order to get the first country specified
+        if (';' in country):
+            country_array = country.split(';')
+            country = country_array[0]
+        
+        if (country not in self.capital_coordinates_df['CountryName'].values):
+            print(self.capital_coordinates_df.shape, country, 'not found in capital_coordinates.csv file')
+            self.latitude = -1
+            self.longitude = -1
+        else:
+            filtered_country_df = self.capital_coordinates_df[self.capital_coordinates_df['CountryName'] == country]
+            self.latitude = filtered_country_df['CapitalLatitude'].values[0]
+            self.longitude = filtered_country_df['CapitalLongitude'].values[0]
+
+    
+    def getDescription(self):
+        return self.name + ' / ' + self.type + ' / ' + self.country
+        
+    
+        
+        
+        
             
 
 class SingleRelationship:
@@ -128,7 +178,7 @@ class ItemNetwork:
             out_node_id = out_item.node_id
             out_name = out_item.name
             out_country = out_item.country
-            out_type = out_item.country
+            out_type = out_item.type
             
             new_df_line = pd.Series([in_node_id, in_name, in_country, in_type, relationship_elem.relationship, out_node_id,\
                                      out_name, out_country, out_type], index = output_columns)
@@ -138,7 +188,64 @@ class ItemNetwork:
         
         
         
+    def getMap(self):
+        folium_map = folium.Map(location=[0, 0], zoom_start =2, min_zoom=2)
+        feature_group = folium.map.FeatureGroup()
+        
+        color_dic = {'central_contour': '#ff0000', 'periph_contour': '#3388ff',\
+                     'Entity': '#02f71a', 'Officer': '#01eef7', 'Intermediary': '#e6f701', 'Address': '#ffffff'}
+
+        printed_node_id_list = [self.central_item.node_id]
+        printed_coordinates = [self.central_item.latitude, self.central_item.longitude]
+        
+        for relationship_elem in self.relationship_list:
+            item_pair = [(relationship_elem.in_item, relationship_elem.out_item)]
+            
+            for item in item_pair:
+                if (item.node_id not in printed_node_id_list) and (item.country != -1) and (item.type != 'Address'):
+                    current_latitude = item.latitude
+                    current_longitude = item.longitude
+                    if (current_latitude, current_longitude) in printed_coordinates:
+                        print('Wooooooola')
+                        radius = 2
+                        coordinates[0] = coordinates[0] + radius * math.sin(2 * math.pi * random.random())
+                        coordinates[1] = coordinates[1] + radius * math.cos(2 * math.pi * random.random())
+                    else:
+                        printed_coordinates += [coordinates]
+                        
+                    feature_group.add_child(
+                        folium.features.CircleMarker(
+                            [coordinates[0], coordinates[1]],
+                            radius=8,
+                            color=color_dic['periph_contour'],
+                            fill_color=color_dic[item.type],
+                            fill = True,
+                            popup = item.getDescription(),
+                            fill_opacity=0.6)
+                        )
+                    printed_node_id_list += [item.node_id]
+
+        # central_item marker
+        feature_group.add_child(
+                    folium.features.CircleMarker(
+                        [self.central_item.latitude, self.central_item.longitude],
+                        radius=8,
+                        color=color_dic['central_contour'],
+                        fill_color=color_dic[self.central_item.type],
+                        fill = True,
+                        popup = self.central_item.getDescription(),
+                        fill_opacity=0.6)
+                    )   
+                    
+                  
+        folium_map.add_child(feature_group)
+        
+        return folium_map
         
         
+
+
+class FileNotReadException(Exception):
+    pass
         
         
